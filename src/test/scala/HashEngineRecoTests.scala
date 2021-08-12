beef799b4248911598b5ff26e2049e86e993f2b4
Bryan Banta <32536327+BryanVtW123@users.noreply.github.com>
2021-08-11 17:35:28 -0700

    Update HashEngineRecoTests.scala
    test to match vivado output

--- src/test/scala/HashEngineRecoTests.scala
+++ src/test/scala/HashEngineRecoTests.scala
@@ -7,7 +7,33 @@ import testutil._
 
 
 class HashEngineRecoTestsU(c: HashEngineReco) extends PeekPokeTester(c) {
-	poke(c.io.M,0)
+	poke(c.io.opMode,0)
+	poke(c.io.SWout,3)
+	poke(c.io.finalMessage,0)
+	poke(c.io.pushData,1)
+	step(1)
+	poke(c.io.Fifoaccept,1)
+	poke(c.io.M,BigInt("8000000000000000",16))
+	step(1)
+	poke(c.io.Fifoaccept,0)
+	step(1)
+	poke(c.io.finalMessage,1)
+	step(200)
+	expect(c.io.done,1)
+	
+	poke(c.io.SWout,0)
+	expect(c.io.H,BigInt("7346BC14F036E87A",16))
+	step(5)
+	poke(c.io.SWout,1)
+	expect(c.io.H,BigInt("E03D0997913088F5",16))
+	step(5)
+	poke(c.io.SWout,2)
+	expect(c.io.H,BigInt("F68411434B3CF8B5",16))
+	step(5)
+	poke(c.io.SWout,3)	
+	expect(c.io.H,BigInt("4FA796A80D251F91",16))
+	step(5)
+
 }
 
 
